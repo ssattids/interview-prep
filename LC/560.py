a9534409ccbc751054e6621645564deb0efde6e4
Salar Satti <salarsatti@salars-mbp-2.lan>
2024-06-02 19:33:02 -0400

    Implement from scratch

--- LC/560.py
+++ LC/560.py
@@ -1,6 +1,29 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        """
+            Implement solution from scratch O(n)
+        """
+        cumalitive_sum = 0
+        prefix_sums = {0:1}
+        result_counts = 0
 
+        for num in nums:
+
+            cumalitive_sum += num
+            diff = cumalitive_sum-k
+            result_counts += prefix_sums.get(diff, 0)
+
+            if cumalitive_sum in prefix_sums:
+                prefix_sums[cumalitive_sum] += 1
+            else:
+                prefix_sums[cumalitive_sum] = 1
+
+        return result_counts
+
+    def subarraySum_solutioncopy(self, nums: List[int], k: int) -> int:
+        """
+            Copy online solution
+        """
         result_ks = 0
         cumalitive_sum = 0
         prefix_sums = {0 : 1}
@@ -15,6 +38,9 @@ class Solution:
         return result_ks
 
     def subarraySum_cumsum(self, nums: List[int], k: int) -> int:
+        """ 
+            Bruteforce solution with optimization: O(n^2)
+        """
         answers = 0
         for i in range(len(nums)):
             total_sum = 0
@@ -27,6 +53,9 @@ class Solution:
 
 
     def subarraySum_bruteforce(self, nums: List[int], k: int) -> int:
+        """
+            Bruteforce solution: O(n^3)
+        """
         answers=0
         for i in range(len(nums)):
             for j in range(len(nums)+1):
