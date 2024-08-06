ffde5ae04617a8b0771379848e2c0c88184d7432
Salar Satti <salarsatti@salars-mbp-2.lan>
2024-08-06 13:06:36 -0400

    LC/20.py

--- LC/20.py
+++ LC/20.py
@@ -1,5 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        stack = []
+
+        mapping = { ")":"(", "}":"{", "]":"["}
+
+        for char in s:
+            if char in mapping: # if closing brace
+                if stack:
+                    top_element = stack.pop()
+                else:
+                    top_element = "#"
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+
+    def isValidFirst(self, s: str) -> bool:
         """
         time complexity: O(n)
         space complexity: O(n)
